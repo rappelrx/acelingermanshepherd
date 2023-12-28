a93fd1c46674fa91a097f019a475dde0af1670ad
Rappel R <rricafor@ucsd.edu>
2023-12-27 23:43:40 -0800

    fixed relative links

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ class NavComponent extends HTMLElement {
     this.innerHTML = `
     <nav class="navbar navbar-expand-md bg-dark-subtle bg-gradient">
         <div class="container-fluid px-5">
-            <a class="navbar-brand" href="/">Acelin German Shepherds</a>
+            <a class="navbar-brand" href="./">Acelin German Shepherds</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -12,10 +12,10 @@ class NavComponent extends HTMLElement {
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav px-4">
                     <li class="nav-item p-2">
-                        <a class="nav-link" href="/">Home</a>
+                        <a class="nav-link" href="./">Home</a>
                     </li>
                     <li class="nav-item p-2">
-                        <a class="nav-link" href="/about.html">About</a>
+                        <a class="nav-link" href="./about.html">About</a>
                     </li>
                     <li class="nav-item p-2 dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -23,14 +23,14 @@ class NavComponent extends HTMLElement {
                             Dogs for Sale
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/puppies.html">Puppies</a></li>
-                            <li><a class="dropdown-item" href="/studs.html">Studs</a></li>
-                            <li><a class="dropdown-item" href="/dams.html">Dams</a></li>
-                            <li><a class="dropdown-item" href="/adults.html">Adult Dogs</a></li>
+                            <li><a class="dropdown-item" href="./puppies.html">Puppies</a></li>
+                            <li><a class="dropdown-item" href="./studs.html">Studs</a></li>
+                            <li><a class="dropdown-item" href="./dams.html">Dams</a></li>
+                            <li><a class="dropdown-item" href="./adults.html">Adult Dogs</a></li>
                         </ul>
                     </li>
                     <li class="nav-item p-2">
-                        <a class="nav-link" href="/contact.html">Contact</a>
+                        <a class="nav-link" href="./contact.html">Contact</a>
                     </li>
                 </ul>
             </div>
